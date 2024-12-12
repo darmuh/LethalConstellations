0eb86990198a5442a284db4a2234236d54247f21
darmuh <darmuh8@gmail.com>
2024-12-11 19:48:37 -0800

    0.2.8

--- LethalConstellations/PluginCore/Plugin.cs
+++ LethalConstellations/PluginCore/Plugin.cs
@@ -10,7 +10,7 @@ namespace LethalConstellations
 {
     [BepInPlugin("com.github.darmuh.LethalConstellations", "LethalConstellations", (PluginInfo.PLUGIN_VERSION))]
     [BepInDependency("imabatby.lethallevelloader", "1.3.8")]
-    [BepInDependency("darmuh.OpenLib", "0.2.10")]
+    [BepInDependency("darmuh.OpenLib", "0.2.11")]
 
     public class Plugin : BaseUnityPlugin
     {
@@ -19,7 +19,7 @@ namespace LethalConstellations
         {
             public const string PLUGIN_GUID = "com.github.darmuh.LethalConstellations";
             public const string PLUGIN_NAME = "LethalConstellations";
-            public const string PLUGIN_VERSION = "0.2.7";
+            public const string PLUGIN_VERSION = "0.2.8";
         }
 
         internal static ManualLogSource Log;
